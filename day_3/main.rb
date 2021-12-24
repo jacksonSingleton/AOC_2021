55377ef9b8428db302115a122e612df365d9c2d6
Jackson Singleton <jackson@tiberon.co>
2021-12-24 09:38:02 -0600

    completed day 3, start day 4

--- day_3/main.rb
+++ day_3/main.rb
@@ -1,5 +1,3 @@
-
-
 $input = File.readlines('input.txt')
 
 
@@ -37,29 +35,62 @@ def part_1
 end
 
 
-def filter_array (arr, filter)
-        filter.each_char.with_index do | bit, idx|
-            if arr.length === 1
-                break
-            end
-            arr = arr.select { | pwr | pwr[idx] == bit}
-            #puts "Number of Possible Solutions: #{arr.length}\nIterations: #{idx + 1}"
-        end
-        return arr[0]
+def find_most_common_bit (arr, bit)
+  zeroes = 0
+  ones = 0
+  arr.each do | line |
+    if line[bit] == "0"
+      zeroes += 1
+    elsif line[bit] == "1"
+      ones += 1
+    end
+  end
+  if zeroes == ones || ones > zeroes
+    return "1"
+  elsif zeroes > ones
+    return "0"
+  end
 end
+
+def find_least_common_bit (arr, bit)
+  zeroes = 0
+  ones = 0
+  arr.each do | line |
+    if line[bit] == "0"
+      zeroes += 1
+    elsif line[bit] == "1"
+      ones += 1
+    end
+  end
+  if zeroes == ones || ones > zeroes
+    return "0"
+  elsif zeroes > ones
+    return "1"
+  end
+end
+
+def filter_array (arr, most_common)
+  for bit in 0..$input[0].length - 2 do
+    if arr.length == 1
+      break
+    end
+    if most_common
+      arr = arr.select{|byte| byte[bit] == find_most_common_bit(arr, bit)}
+    else
+      arr = arr.select{|byte| byte[bit] == find_least_common_bit(arr, bit)}
+    end
+  end
+  return arr[0]
+end
+
 def part_2
     #TODO:
         # Find and keep numbers based on most common bit in each position for Oxygen levels
         # Find and keep numbers based on the least common bit in each position for the CO2 Scrubber Levels
         # Multiply levels to find life support amt.
         # We can use Gamma and Epsilon as a filter 
-        filters = part_1
-        gamma = filters[0]
-        epsilon = filters[1]
-
-        oxygen_generator = filter_array($input.clone, gamma)
-        co2_scrubber = filter_array($input.clone, epsilon)
- 
+        oxygen_generator = filter_array($input.clone, true)
+        co2_scrubber = filter_array($input.clone, false)
         puts "Oxygen Generator: #{oxygen_generator}"    
         puts "CO2 Scrubber: #{co2_scrubber}"    
         puts "Product: #{oxygen_generator.to_i(2) * co2_scrubber.to_i(2)}"
---
+++ day_4/main.rb
@@ -0,0 +1,5 @@
+
+
+$input = File.readlines("input.txt")
+
+$boards
